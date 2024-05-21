15d3216457df540d4c2ef8f1833f7211edcdab9c
maulanaraka <maulanaraka611@gmail.com>
2024-05-21 15:35:23 +0700

    fix : dashboard

--- resources/views/Organizer/dashboard.blade.php
+++ resources/views/Organizer/dashboard.blade.php
@@ -10,7 +10,7 @@
 
     {{-- heading --}}
     <div class="relative overflow-hidden bg-cover bg-no-repeat p-12 text-center h-screen max-w-screen"
-        style="background-image: url('{{ asset('#') }}')">
+        style="background-image: url('{{ asset('https://www.ctvnews.ca/polopoly_fs/1.5209840.1648432195!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg') }}')">
         <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed"
             style="background-color: rgba(18, 24, 46, 0.7)"></div>
         <div class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-fixed mt-10">
@@ -21,12 +21,8 @@
                     </p>
                     <h1
                         class="mb-8 text-4xl font-bold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
-                        PhilanthroPal</h1>
-                    {{-- <a href="#"
-                        class="rounded-xl border-2 border-neutral-50 px-3 pb-[8px] pt-[10px] text-lg lg:text-md sm:px-10 font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200"
-                        data-te-ripple-init data-te-ripple-color="light">
-                        Pesan Sekarang
-                    </a> --}}
+                        PhilanthroPal
+                    </h1>
                 </div>
             </div>
         </div>
