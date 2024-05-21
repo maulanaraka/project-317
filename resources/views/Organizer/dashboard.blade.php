6f1525f577e26769fc9d623cce81f06eacab66f2
maulanaraka <maulanaraka611@gmail.com>
2024-05-21 15:31:21 +0700

    fix: dashboard

--- resources/views/Organizer/dashboard.blade.php
+++ resources/views/Organizer/dashboard.blade.php
@@ -137,162 +137,85 @@
     </div>
     {{-- carousel end --}}
 
-    {{-- table --}}
-    <div class="pt-8">
-        <div class="flex px-4 place-content-between">
-            <h3 class="text-gray-700 text-3xl font-medium">Tables</h3>
-
-            <div class="mt-3 flex flex-col sm:flex-row">
-                <div class="flex">
-                    <div class="relative">
-                        <select
-                            class="appearance-none h-full rounded-l border block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500">
-                            <option>5</option>
-                            <option>10</option>
-                            <option>20</option>
-                        </select>
-
-                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                        </div>
-                    </div>
-
-                    <div class="relative">
-                        <select
-                            class="appearance-none h-full rounded-r border-t sm:rounded-r-none sm:border-r-0 border-r border-b block appearance-none w-full bg-white border-gray-400 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-l focus:border-r focus:bg-white focus:border-gray-500">
-                            <option>All</option>
-                            <option>Active</option>
-                            <option>Inactive</option>
-                        </select>
-
-                        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                            <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="block relative mt-2 sm:mt-0">
-                    <span class="absolute inset-y-0 left-0 flex items-center pl-2">
-                        <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
-                            <path
-                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
-                            </path>
-                        </svg>
-                    </span>
-
-                    <input placeholder="Search"
-                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
-                </div>
-            </div>
-        </div>
-
-        <div class="max-w-full px-4 py-4 overflow-x-auto">
-            <div class="inline-block min-w-full shadow rounded-lg overflow-hidden">
-                <table class="min-w-full leading-normal">
-                    <thead>
-                        <tr>
-                            <th
-                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                User</th>
-                            <th
-                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Rol</th>
-                            <th
-                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Created at</th>
-                            <th
-                                class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                                Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <div class="flex items-center">
-                                    <div class="flex-shrink-0 w-10 h-10">
-                                        <img class="w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                            alt="" />
-                                    </div>
-
-                                    <div class="ml-3">
-                                        <p class="text-gray-900 whitespace-no-wrap">Vera Carpenter</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">Admin</p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">Jan 21, 2020</p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                    <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                    <span class="relative">Activo</span>
-                                </span>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <div class="flex items-center">
-                                    <div class="flex-shrink-0 w-10 h-10">
-                                        <img class="w-full h-full rounded-full"
-                                            src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-                                            alt="" />
-                                    </div>
-                                    <div class="ml-3">
-                                        <p class="text-gray-900 whitespace-no-wrap"> Blake Bowman</p>
-                                    </div>
-                                </div>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">Editor</p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <p class="text-gray-900 whitespace-no-wrap">Jan 01, 2020</p>
-                            </td>
-                            <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                <span class="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
-                                    <span aria-hidden class="absolute inset-0 bg-green-200 opacity-50 rounded-full"></span>
-                                    <span class="relative">Activo</span>
-                                </span>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div class="px-5 py-5 bg-white border-t flex flex-col xs:flex-row items-center xs:justify-between">
-                    <span class="text-xs xs:text-sm text-gray-900">Showing 1 to 4 of 50 Entries</span>
-
-                    <div class="inline-flex mt-2 xs:mt-0">
-                        <button
-                            class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-l">Prev</button>
-                        <button
-                            class="text-sm bg-gray-300 hover:bg-gray-400 text-gray-800 font-semibold py-2 px-4 rounded-r">Next</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="py-8 px-8">
+        <button class="hover:bg-sky-500 bg-sky-700 text-white font-bold py-2 px-4 rounded-xl">
+            Lihat Event Lainnya
+        </button>
     </div>
 
-    <div class="w-1/2 h-1/2 m-auto border-2 border-black p-32">
-        @if (session('username'))
-            <h1 class="text-center">Welcome {{ session('username') }}</h1>
-        @endif
+    <div class="py-4 px-8">
+        <h3 class="text-gray-700 text-3xl font-medium">Category</h3>
+    </div>
 
-        <form action="/organizer/logout" method="POST">
-            @csrf
-            @method('delete')
-            <button type="submit" class="w-14 h-10 border-2 border-black text-center">Logout</button>
-        </form>
-
-        <div class="flex flex-wrap w-full mt-10 space-x-4">
-            <a href="/organizer/formAddEvent" class="border-2 border-black">Add Event</a>
-            <a href="/organizer/listMyEvent" class="border-2 border-black">My Event</a>
+    <div class="py-8 px-8">
+        <div class="grid grid-cols-2 gap-8">
+            <!-- Card 1 -->
+            <div class="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm flex items-center">
+                <img src="https://www.binghamton.edu/news/images/uploads/features/food-bank-southern-tier.jpg" alt="Image" class="p-2 max-w-40 max-h-40 rounded-lg flex mr-4">
+                <div>
+                    <a href="#" class="block">
+                        <h5 class="mb-2 text-2xl font-bold text-gray-900">Lingkungan</h5>
+                    </a>
+                    <p class="mb-3 text-sm text-gray-700">Lingkungan Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a fugiat magni corrupti, fuga beatae mollitia exercitationem magnam eius nemo natus animi, perspiciatis quisquam saepe aliquid qui dolor esse placeat.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            
+            <!-- Card 2 -->
+            <div class="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm flex items-center">
+                <img src="https://www.binghamton.edu/news/images/uploads/features/food-bank-southern-tier.jpg" alt="Image" class="p-2 max-w-40 max-h-40 rounded-lg flex mr-4">
+                <div>
+                    <a href="#" class="block">
+                        <h5 class="mb-2 text-2xl font-bold text-gray-900">Technology</h5>
+                    </a>
+                    <p class="mb-3 text-sm text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a fugiat magni corrupti, fuga beatae mollitia exercitationem magnam eius nemo natus animi, perspiciatis quisquam saepe aliquid qui dolor esse placeat.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            
+            <!-- Card 3 -->
+            <div class="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm flex items-center">
+                <img src="https://www.binghamton.edu/news/images/uploads/features/food-bank-southern-tier.jpg" alt="Image" class="p-2 max-w-40 max-h-40 rounded-lg flex mr-4">
+                <div>
+                    <a href="#" class="block">
+                        <h5 class="mb-2 text-2xl font-bold text-gray-900">Education</h5>
+                    </a>
+                    <p class="mb-3 text-sm text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a fugiat magni corrupti, fuga beatae mollitia exercitationem magnam eius nemo natus animi, perspiciatis quisquam saepe aliquid qui dolor esse placeat.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            
+            <!-- Card 4 -->
+            <div class="max-w-full p-6 bg-white border border-gray-200 rounded-lg shadow-sm flex items-center">
+                <img src="https://www.binghamton.edu/news/images/uploads/features/food-bank-southern-tier.jpg" alt="Image" class="p-2 max-w-40 max-h-40 rounded-lg flex mr-4">
+                <div>
+                    <a href="#" class="block">
+                        <h5 class="mb-2 text-2xl font-bold text-gray-900">Health</h5>
+                    </a>
+                    <p class="mb-3 text-sm text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum a fugiat magni corrupti, fuga beatae mollitia exercitationem magnam eius nemo natus animi, perspiciatis quisquam saepe aliquid qui dolor esse placeat.</p>
+                    <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                        Read more
+                        <svg class="w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                        </svg>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 
