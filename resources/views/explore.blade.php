11fbdf8244261fdb224033fcfbf9b6bb7c099ddc
Tomen1980 <agungjumantoroandrian@gmail.com>
2024-05-27 22:53:45 +0700

    + Config JS + Membuat tampilan explore + Membuat Tambahan Landing + Membuat section baru pada navbar

--- resources/views/explore.blade.php
+++ resources/views/explore.blade.php
@@ -8,7 +8,7 @@
     @include('Layout.navbar')
     {{-- End Navbar --}}
 
-    <div class="w-1/2 h-1/2 mx-auto mt-40 border-2 border-black p-2">
+    <div class="container mx-auto mt-40 p-4">
         @if (Session::has('error'))
             <p class="text-red-500">{{ Session::get('error') }}</p>
         @endif
@@ -27,34 +27,55 @@
             </div>
         </form>
 
-        @forelse ($allEvent as $event)
-            <div class="w-[60%] h-1/2 m-auto border-2 border-black">
-                <ul class="text-center">
-                    <li>Title : {{ $event->title }}</li>
-                    <li>Deskripsi : {{ $event->description }}</li>
-                    <li>Tgl_dimulai : {{ $event->event_date }}</li>
-                    <li>Category : {{ $event->category_name }}</li>
-                    <div class="w-[100px] h-[100px] m-auto">
-                        <img src="{{ Storage::url('event/') . $event->media }}"
-                            class="w-full h-full object-cover rounded-full">
-                    </div>
-                    <li>User : {{ !$event->community_id == null ? 'Community' : 'Organizer' }}</li>
+        <div class="my-5 space-y-2">
+            <a href="/" class="p-2 text-md bg-brand-secondary text-white font-semibold rounded-md">< Kembali</a>
+            <h1 class="font-bold text-4xl text-brand-secondary">Events</h1>
+            <p class="font-bold text-2xl text-brand-gray">Lihat event-event yang kami asosiasikan!</p>
+        </div>
 
-                    <div class="flex justify-center gap-3">
-                        @if ($event->community_id == null)
-                            <a href="/community/{{ $event->id }}/detailEvent"
-                                class="w-14 h-10 border-2 border-black text-center bg-lime-200">Detail</a>
-                        @elseif ($event->organizer_id == null)
-                            <a href="/organizer/{{ $event->id }}/detailEvent"
-                                class="w-14 h-10 border-2 border-black text-center bg-lime-200">Detail</a>
-                        @endif
-                    </div>
-                </ul>
-            </div>
-        @empty
-            <h1 class="text-center text-4xl">Belum ada event yang bisa dijalankan</h1>
-        @endforelse
+        <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            
+            @forelse ($allEvent as $item)
+                @if ($item->organizer_id !== null)
+                    <a href="/organizer/dashboard/event/{{ $item->id }}">
+                    @elseif ($item->community_id !== null)
+                        <a href="/community/dashboard/event/{{ $item->id }}">
+                @endif
+                <div class="relative w-full h-[400px] overflow-hidden rounded-lg border border-gray-300">
+                    <img src="{{ Storage::url('event/') . $item->media }}" class="w-full h-full object-cover"
+                        alt="kegiatan-1" />
 
-        {{ $allEvent->links() }}
+                    <div class="w-full h-full bg-black/50 absolute inset-0 rounded-lg z-10">
+                        <span
+                            class="absolute block top-0 left-0 text-xl font-semibold text-white bg-brand-secondary-lighter rounded-tl-lg rounded-br-lg px-6 py-2 z-20">
+                            <p>{{ $item->category_name }}</p>
+                        </span>
+
+                        <div class="absolute bottom-0 p-4 space-y-1.5 w-full">
+                            <h2 class="text-3xl font-semibold text-white">
+                                {{ $item->title }}
+                            </h2>
+                            <div class="flex items-center bg-brand-secondary-lighter relative rounded-lg w-fit px-8 py-2">
+                                <div
+                                    class="bg-brand-primary absolute left-0 flex items-center justify-center h-full w-10 rounded-tl-lg rounded-bl-lg">
+                                    <img alt="h-6 w-6 shrink-0" src="/icons/group.svg" />
+                                </div>
+
+                                <p class="text-lg ml-8 font-semibold text-white">{{ $item->event_date }}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </a>
+            @empty
+                <h1 class="text-center text-4xl">Belum ada event yang bisa dijalankan</h1>
+            @endforelse
+        </div>
+
+        <div class="mt-8">
+            {{ $allEvent->links() }}
+        </div>
     </div>
+
+    @include('../Layout.footer')
 @endsection
