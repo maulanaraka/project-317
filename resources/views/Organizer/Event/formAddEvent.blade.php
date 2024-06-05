e8bbdb0c9fc1fe18f4deb9cab4fca6eb003afe68
maulanaraka <maulanaraka611@gmail.com>
2024-06-05 14:32:22 +0700

    feat : addevent page fornt end oraganizer/community

--- resources/views/Organizer/Event/formAddEvent.blade.php
+++ resources/views/Organizer/Event/formAddEvent.blade.php
@@ -4,56 +4,83 @@
 
 @section('content')
 
-  {{-- Navbar --}}
-  @include('Layout.navbar')
-  {{-- End Navbar --}}
+    {{-- Navbar --}}
+    @include('Layout.navbar')
+    {{-- End Navbar --}}
 
-    <div class="bg-transparent-gray-100 px-4 my-32 max-2-3xl mx-auto space-y-6 w-1/2 rounded">
-
-        <h1 class="ml-4 text-2xl font-semibold text-center font-fredoka z-20">Add Event</h1>
-
-        @if ($errors->any())
-            <div class="w-full h-full m-auto bg-red-300">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
+    <div class="pt-20">
+        <div class="relative bg-zinc-100 overflow-hidden w-full max-w-5xl mx-auto my-16 p-8 rounded-3xl shadow">
+            <div class="mb-8">
+                <p class="text-6xl font-medium text-stone-400 text-justify font-['Fredoka']">New Event</p>
             </div>
-        @endif
-        
-        @if (Session::has('error'))
-            <p class="text-red-500">{{ Session::get('error') }}</p>
-        @endif
-        @if (Session::has('success'))
-            <p class="text-green-500">{{ Session::get('success') }}</p>
-        @endif
 
+            @if ($errors->any())
+                <div class="bg-red-300 p-4 rounded">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-        <form action="/organizer/addEvent" method="POST" enctype="multipart/form-data">
-            @csrf
-            @method('post')
+            @if (Session::has('error'))
+                <p class="text-red-500">{{ Session::get('error') }}</p>
+            @endif
+            @if (Session::has('success'))
+                <p class="text-green-500">{{ Session::get('success') }}</p>
+            @endif
 
-            <div class="w-1/2 m-auto space-y-4">
-                <input type="title" name="title" id="title" placeholder="title"
-                class="w-3/4 h-5 border-2 border-black"><br>
-                
-            <textarea class="border-2 border-black" name="description" id="" cols="30" rows="10"></textarea><br>
+            <form action="/organizer/addEvent" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('post')
 
-            <input type="date" name="event_date" id="event_date" placeholder="event_date"
-                class="w-3/4 h-5 border-2 border-black"><br>
+                <div class="space-y-4">
+                    <div>
+                        <label for="title" class="block text-base font-semibold text-zinc-500 font-fredoka">Title</label>
+                        <input type="text" name="title" id="title" placeholder="Title"
+                            class="mt-1 border bg-zinc-600 block w-full py-2 px-4 rounded-lg placeholder:text-white text-white font-fredoka">
+                    </div>
 
-            <input type="file" name="media" class=""> <br>
+                    <div>
+                        <label for="description"
+                            class="block text-base font-semibold text-zinc-500 font-fredoka">Description</label>
+                        <textarea name="description" id="description" placeholder="Description"
+                            class="mt-1 border border-zinc-600 block w-full py-2 px-4 rounded-lg font-fredoka" rows="4"></textarea>
+                    </div>
 
-            <select name="event_category" id="cars">
-                @foreach ($category_list as $cl ):
-                <option value="{{ $cl->id }}">{{$cl->category_name}}</option>
-                    
-                @endforeach
-                </select><br>
+                    <div>
+                        <label for="event_date" class="block text-base font-semibold text-zinc-500 font-fredoka">Event
+                            Date</label>
+                        <input type="date" name="event_date" id="event_date"
+                            class="mt-1 border border-zinc-600 block w-full py-2 px-4 rounded-lg font-fredoka">
+                    </div>
 
-                <button type="submit" class="w-14 h-10 border-2 border-black text-center ">Kirim</button>
-            </div>
-        </form>
+                    <div>
+                        <label for="media" class="block text-base font-semibold text-zinc-500 font-fredoka">Media</label>
+                        <input type="file" name="media" id="media"
+                            class="mt-1 block w-full py-2 px-4 rounded-lg bg-zinc-300 font-fredoka">
+                    </div>
+
+                    <div>
+                        <label for="event_category"
+                            class="block text-base font-semibold text-zinc-500 font-fredoka">Category</label>
+                        <select name="event_category" id="event_category"
+                            class="mt-1 block w-full py-2 px-4 rounded-lg bg-yellow-600 text-white font-fredoka">
+                            @foreach ($category_list as $cl)
+                                <option value="{{ $cl->id }}">{{ $cl->category_name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+
+                    <div class="flex justify-end">
+                        <button type="submit"
+                            class="bg-sky-900 text-white py-2 px-6 rounded-md font-extrabold font-['Plus Jakarta Sans'] hover:bg-sky-700 focus:outline-none">Post</button>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
+
+    @include('Layout.footer')
 @endsection
