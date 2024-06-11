36346aeffb14918402f31431335bcdb528f8f26e
maulanaraka <maulanaraka611@gmail.com>
2024-06-11 11:59:47 +0700

    front end

--- resources/views/Organizer/detailEventDashboard.blade.php
+++ resources/views/Organizer/detailEventDashboard.blade.php
@@ -8,21 +8,28 @@
     @include('Layout.navbar')
     {{-- End Navbar --}}
 
-    <div class="w-1/2 h-1/2 mx-auto mt-40 border-2 border-black p-2">
+    <div class="pt-20">
+        <div class="relative bg-zinc-100 overflow-hidden w-full max-w-5xl mx-auto my-16 p-8 rounded-3xl shadow">
+            <div class="mb-8">
+                <p class="text-6xl font-medium text-stone-400 text-justify font-['Fredoka']">{{ $event->title }}</p>
+            </div>
 
-        <div class="w-[60%] h-1/2 m-auto border-2 border-black">
-            <ul class="text-center">
-                <li>Title : {{ $event->title }}</li>
-                <li>Deskripsi : {{ $event->description }}</li>
-                <li>Tgl_dimulai : {{ $event->event_date }}</li>
-                <li>Category : {{ $event->category_name }}</li>
-                <li>username : {{ $event->username }}</li>
-                <li>WA : <a href="https://wa.me/{{ $event->phone }}" target="_blank">{{ $event->phone }}</a></li>
-                <div class="w-[100px] h-[100px] m-auto">
-                    <img src="{{ Storage::url('event/') . $event->media }}" class="w-full h-full object-cover rounded-full">
-                </div>
-            </ul>
+            <div class="bg-white p-6 rounded-lg shadow-md">
+                <ul class="space-y-4">
+                    <li class="text-lg font-semibold text-zinc-500 font-fredoka">Deskripsi : <span
+                        class="font-normal">{{ $event->description }}</span></li>
+                    <li class="text-lg font-semibold text-zinc-500 font-fredoka">Tgl_dimulai : <span
+                        class="font-normal">{{ $event->event_date }}</span></li>
+                    <li class="text-lg font-semibold text-zinc-500 font-fredoka">Category : <span
+                        class="font-normal">{{ $event->category_name }}</span></li>
+                    <li class="text-lg font-semibold text-zinc-500 font-fredoka">username : <span
+                        class="font-normal">{{ $event->username }}</span></li>
+                    <li class="text-lg font-semibold text-zinc-500 font-fredoka">WA : <a href="https://wa.me/{{ $event->phone }}" target="_blank">{{ $event->phone }}</a></li>
+                    <div class="w-[100px] h-[100px] m-auto">
+                        <img src="{{ Storage::url('event/') . $event->media }}" class="w-full h-full object-cover rounded-full">
+                    </div>
+                </ul>
+            </div>
         </div>
-
     </div>
 @endsection
